076bffc3d1de257425da5a198e489b1a1b7c7c12
theoderic <onipetheoderic@gmail.com>
2019-08-11 03:21:55 +0100

    flexible navigation bar

--- example/src/App.js
+++ example/src/App.js
@@ -6,7 +6,12 @@ export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ExampleComponent googleFont="Baumans" logo_link="https://www.sportradar.com/wp-content/themes/company-themes/sr-main3/img/sportradar-logo.svg" smallCaps={false} fontSize={14} logo_width={250} logo_height={130} fontColor="black" bgColor="white" seperator_index={2.3}>
+        <li>Home</li>
+       <li>About Us</li>
+       <li>Products</li>
+       <li>Who we are</li>
+		</ExampleComponent>        
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component, Children } from 'react'
 import PropTypes from 'prop-types'
 
 import styles from './styles.css'
 
+import WebFont from 'webfontloader';
+import allFonts from './allfont'
+/**/
+WebFont.load({
+  google: {
+    families: allFonts,
+  }
+});
+
+
 export default class ExampleComponent extends Component {
   static propTypes = {
-    text: PropTypes.string
+    logo_link: PropTypes.string,
+    logo_width: PropTypes.number,
+    logo_height: PropTypes.number,
+    fontSize: PropTypes.number,
+    fontColor: PropTypes.string,
+    fontStyle: PropTypes.string,
+    smallCaps: PropTypes.bool,
+    googleFont: PropTypes.string,
+    bgColor: PropTypes.string,
+    aligner: PropTypes.number,
+    seperator_index: PropTypes.number,
   }
 
   render() {
-    const {
-      text
-    } = this.props
+    let caps = this.props.smallCaps;
+    caps = true ? "uppercase" : "lowercase";
+    const lists = Children.map(this.props.children, child => (
+      
+      <li className="horizontal_menu_item" style={{fontFamily:this.props.googleFont, textTransform:caps, fontSize:this.props.fontSize, paddingTop:(this.props.logo_height/this.props.seperator_index), color: this.props.fontColor}}>{child}</li>
+    ));
+
+   
 
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div style={{backgroundColor:this.props.bgColor}}>
+      <ul style={{listStyleType: 'none', margin: 0, padding: 0, overflow: 'hidden'}}>
+     
+        <img className="logo_menu_item" src={this.props.logo_link} style={{width:this.props.logo_width, height: this.props.logo_height}}/>{lists}
+      </ul>
       </div>
     )
   }
