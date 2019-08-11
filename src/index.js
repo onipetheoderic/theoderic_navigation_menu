5e02f7c46adccba284c274d4ff4b3927c21f1c2a
theoderic <onipetheoderic@gmail.com>
2019-08-11 12:30:47 +0100

    responsiveness on mobile node

--- src/index.js
+++ src/index.js
@@ -14,6 +14,13 @@ WebFont.load({
 
 
 export default class ExampleComponent extends Component {
+  constructor() {
+    super();
+      this.state = {
+        normal:true,
+        show: false
+      };
+}
   static propTypes = {
     logo_link: PropTypes.string,
     logo_width: PropTypes.number,
@@ -27,6 +34,18 @@ export default class ExampleComponent extends Component {
     aligner: PropTypes.number,
     seperator_index: PropTypes.number,
   }
+handleOnClick =(condition) => {
+  let presentCondition = condition;
+  if(presentCondition=="on"){
+    this.setState({normal:false, show:true})
+  }
+  else if(presentCondition=="off"){
+    this.setState({normal:true, show: false})
+  }
+}
+
+
+
 
   render() {
     let caps = this.props.smallCaps;
@@ -36,11 +55,38 @@ export default class ExampleComponent extends Component {
       <li className="horizontal_menu_item" style={{fontFamily:this.props.googleFont, textTransform:caps, fontSize:this.props.fontSize, paddingTop:(this.props.logo_height/this.props.seperator_index), color: this.props.fontColor}}>{child}</li>
     ));
 
+    const mLister = Children.map(this.props.children, child => (
+      
+      <li className="innerDiv" style={{fontFamily:this.props.googleFont, textTransform:caps, fontSize:this.props.fontSize, color: 'white'}}>{child}</li>
+    ));
+
    
 
     return (
       <div style={{backgroundColor:this.props.bgColor}}>
-      <ul style={{listStyleType: 'none', margin: 0, padding: 0, overflow: 'hidden'}}>
+{this.state.show &&
+<div class="mobile_menu_items">
+  <ul style={{listStyleType: 'none', padding:0}}>
+   {mLister}
+  </ul>
+</div>
+}
+<img className="logo_mobile" src={this.props.logo_link} style={{width:200, height: 50}}/>
+{this.state.normal &&
+<div class="container" onClick={()=>this.handleOnClick("on")}>
+  <div class="bar1"></div>
+  <div class="bar2"></div>
+  <div class="bar3"></div>
+</div>
+}
+{!this.state.normal &&
+<div class="container" onClick={()=>this.handleOnClick("off")}>
+  <div class="bar1X"></div>
+  <div class="bar2X"></div>
+  <div class="bar3X"></div>
+</div>
+}
+      <ul className="desktopView" style={{listStyleType: 'none', margin: 0, padding: 0, overflow: 'hidden'}}>
      
         <img className="logo_menu_item" src={this.props.logo_link} style={{width:this.props.logo_width, height: this.props.logo_height}}/>{lists}
       </ul>
